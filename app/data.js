f9672fcb7821fef20902c26202aac41570d71482
Kumaravel187 <46121794+Kumaravel187@users.noreply.github.com>
2023-11-07 23:07:36 +0800

    Set up files

---
+++ app/data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [
+  {
+    handle: `@siddohandsome`,
+    profilePic: `images/sid.png`,
+    likes: 27,
+    retweets: 10,
+    tweetText: `When I grow up, I want to be pilot and fight for pakistan!`,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "4b161eee-c0f5-4545-9c4b-8562944223ee",
+  },
+  {
+    handle: `@EmersonCrack`,
+    profilePic: `images/emerson.png`,
+    likes: 6500,
+    retweets: 234,
+    tweetText: `I was today years old when I realised that oranges are meant to be peeled open. WTF. bodoh`,
+    replies: [
+      {
+        handle: `@EdyLeeFuckingHandsome✅`,
+        profilePic: `images/edy.png`,
+        tweetText: `Bro I think you should just shut the fuck up`,
+      },
+      {
+        handle: `@DaddySiddy`,
+        profilePic: `images/dsid.png`,
+        tweetText: `WHERE THE FUCKING THANNI LA CBBBBBB. GIVE ME THE WHISKEY!`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "3c23454ee-c0f5-9g9g-9c4b-77835tgs2",
+  },
+  {
+    handle: `@ZaiCutler`,
+    profilePic: `images/zai.png`,
+    likes: 10,
+    retweets: 3,
+    tweetText: `Turning 30 in December. Yikes ><`,
+    replies: [
+      {
+        handle: `@BanglaDog`,
+        profilePic: `images/asyraf.png`,
+        tweetText: `You look like apek coffeshop`,
+      },
+      {
+        handle: `@Richie`,
+        profilePic: `images/richie.png`,
+        tweetText: `I have no idea on how the heck I ended up here. But then again I am always lost`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "8hy671sff-c0f5-4545-9c4b-1237gyys45",
+  },
+];
---
+++ app/index.js
@@ -0,0 +1,154 @@
+import { tweetsData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+// console.log(uuidv4()); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+
+const getFeedHtml = () => {
+  let feedHTML = ``;
+
+  tweetsData.forEach((tweet) => {
+    let likeIconClass = ``;
+    let retweeetIconClass = ``;
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    }
+
+    if (tweet.isRetweeted) {
+      retweeetIconClass = "retweeted";
+    }
+
+    let repliesHTML = "";
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach((reply) => {
+        repliesHTML += `
+        <div class="tweet-reply">
+          <div class="tweet-inner">
+            <img src="${reply.profilePic}" class="profile-pic">
+              <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+              </div>
+          </div>
+        </div>`;
+
+        // console.log(repliesHTML);
+      });
+    }
+
+    feedHTML += `
+    <div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}" ></i>
+                        <p class="test">${tweet.replies.length}</p>
+                    </span>
+                    <span class="tweet-detail">
+                        <i class="fa-solid fa-heart ${likeIconClass}" data-like="${tweet.uuid}"></i>
+                        <p class="test">${tweet.likes}</p>
+                    </span>
+                    <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet ${retweeetIconClass}" data-retweet="${tweet.uuid}"></i>
+                        <p class="test">${tweet.retweets}</p>
+                    </span>
+                </div>   
+            </div>            
+        </div>
+        <div class="hidden" id="replies-${tweet.uuid}">
+          ${repliesHTML}
+        </div>   
+    </div> 
+    `;
+  });
+
+  return feedHTML;
+};
+
+const render = () => {
+  const feedContent = document.getElementById("feed");
+  feedContent.innerHTML = getFeedHtml();
+};
+
+const handleLikeClick = (tweetId) => {
+  const targetTweetObj = tweetsData.filter((tweets) => {
+    return tweets.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+
+  render();
+};
+
+const handleRetweetClick = (tweetId) => {
+  // console.log(tweetId);
+  const tweetDataObj = tweetsData.filter((tweet) => {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  // console.log(tweetDataObj);
+
+  if (tweetDataObj.isRetweeted) {
+    tweetDataObj.retweets--;
+  } else {
+    tweetDataObj.retweets++;
+  }
+
+  tweetDataObj.isRetweeted = !tweetDataObj.isRetweeted;
+
+  render();
+};
+
+const handleReplyClick = (replyId) => {
+  const reply = document
+    .getElementById(`replies-${replyId}`)
+    .classList.toggle("hidden");
+};
+
+const handleTweetBtnClick = () => {
+  const tweetInput = document.getElementById("tweet-input");
+  if (tweetInput.value) {
+    let tweetObj = {
+      handle: `@Apunehneh`,
+      profilePic: `images/seyan.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    };
+
+    tweetsData.unshift(tweetObj);
+
+    tweetInput.value = ``;
+
+    render();
+  } else {
+    window.alert("Please type in a legitimate tweet");
+  }
+};
+
+render();
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === `tweet-btn`) {
+    handleTweetBtnClick();
+  }
+});
